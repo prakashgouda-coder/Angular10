d1c6fd0f0458c34b904c0daa5ca1fa6101c8499c
Prakash Gouda <prakashbik@outlook.com>
2020-10-20 00:16:44 +0530

    Add project files.

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{CommonModule} from '@angular/common';
+import{FormsModule} from '@angular/forms'
+
+import{InputTextModule} from 'primeng/inputtext';
+import{CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CategoryComponent,
+    ProductComponent
+  ],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    BrowserModule,
+    FormsModule,
+     
+    AppRoutingModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
